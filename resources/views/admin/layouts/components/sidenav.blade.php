163462e09daa53550658aedee56926c5190ec832
arindam-revo <hsdevnath@gmal.com>
2021-11-23 18:50:24 +0600

    Created dashboard

---
+++ resources/views/admin/layouts/components/sidenav.blade.php
@@ -0,0 +1,33 @@
+<nav class="pcoded-navbar">
+                        <div class="pcoded-inner-navbar main-menu">
+                            <div class="pcoded-navigatio-lavel">Navigation</div>
+                            <ul class="pcoded-item pcoded-left-item">
+                                <li class="pcoded-hasmenu active pcoded-trigger">
+                                    <a href="javascript:void(0)">
+                                        <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+                                        <span class="pcoded-mtext">Dashboard</span>
+                                    </a>
+                                    <ul class="pcoded-submenu">
+                                        <li class="">
+                                            <a href="dashboard">
+                                                <span class="pcoded-mtext">Dashboard</span>
+                                            </a>
+                                        </li>
+                                      <!--  <li class="">
+                                            <a href="dashboard-crm.htm">
+                                                <span class="pcoded-mtext">CRM</span>
+                                            </a>
+                                        </li>
+                                        <li class="active">
+                                            <a href="dashboard-analytics.htm">
+                                                <span class="pcoded-mtext">Analytics</span>
+                                                <span class="pcoded-badge label label-info ">NEW</span>
+                                            </a>
+                                        </li>-->
+                                    </ul>
+                                </li>
+                               
+                              
+                              
+                        </div>
+                    </nav>
